7f9f16dcf610592ee8eeb14dc7af29e74109d9bb
asharanjith <ashasvijayan@gmail.com>
2022-11-01 23:27:52 +0400

    Add sample array for displaying the list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.css';
+import listInput from './modules/listInput';
+
+const listView=document.querySelector('.list');
+
+const Tasks=[
+    {
+        index: 1,
+        description: 'Buy newspaper',
+        completed: false,
+    },
+    {
+        index: 2,
+        description: 'Buy milk',
+        completed: false,
+    },
+    {
+        index: 3,
+        description: 'Buy eggs',
+        completed: false,
+    },
+  ]; 
+
+  listInput(listView,Tasks);
+
+
